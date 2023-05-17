e9acec19d42d0befcc624451a82fa1495656848b
Liam Connell <connell.liam@bcg.com>
2023-05-17 17:48:34 -0400

    set up cli

--- coder/tasks.py
+++ coder/tasks.py
@@ -3,7 +3,7 @@ from coder import vectorstore, ingesters, utils
 
 def store_directory(path):
     v = vectorstore.VectorStore()
-    docs = ingesters.ingest_code_files("../coder")
+    docs = ingesters.ingest_code_files(path)
     git_hash = utils.get_git_hash()
 
     v.create_collection(git_hash, {"codebase": path})
